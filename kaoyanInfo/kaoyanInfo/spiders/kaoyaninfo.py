3ac208023f598de6a50711dfa73e17ed673ea789
taoxianpeng <492350627@qq.com>
2020-04-06 19:35:58 +0800

    增加注释

--- kaoyanInfo/kaoyanInfo/spiders/kaoyaninfo.py
+++ kaoyanInfo/kaoyanInfo/spiders/kaoyaninfo.py
@@ -4,7 +4,6 @@ import re
 
 class KaoyaninfoSpider(scrapy.Spider):
     name = 'kaoyaninfo'
-    #allowed_domains = ['kao.com']
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -13,30 +12,30 @@ class KaoyaninfoSpider(scrapy.Spider):
     def start_requests(self,):
         for n in range(1,389):
             yield scrapy.Request('http://muchong.com/f-430-%d-typeid-2304'%(n),headers=self.headers)
-        # yield scrapy.Request('http://muchong.com/f-430-1-typeid-2304',headers=self.headers)
+        
 
     def parse(self, response):
         #获取页面url
         root_url = 'http://muchong.com'
         for urls in response.xpath('//a[@class="a_subject"]/@href')[1:-1].extract():
             yield scrapy.Request(root_url + urls, headers=self.headers, callback=self.content_parse)
-        # url1 = response.xpath('//a[@class="a_subject"]/@href')[1:-1].extract()[0]
-        # yield scrapy.Request(root_url + url1, headers=self.headers, callback=self.content_parse)
+        
     def content_parse(self,response):
         item = {}
         
-        #item['school'] = response.xpath('//table[@class="adjust_table"/tr[1]/td[2]/text()')
+        
         print('+++++++++++++++++++++++++++++++++++++++++++++++++++++')
         try:
+            #招生学校名字
             print(response.xpath('//table[@class="adjust_table"]/tr[2]/td[2]/text()').extract()[0])
             item['school'] = response.xpath('//table[@class="adjust_table"]/tr[2]/td[2]/text()').extract()[0]
         except IndexError:
             print('error: 未找到学校名字信息')
             item['school'] = 'error: 未找到学校名字信息'
         
-        # print(response.xpath('//table[@class="adjust_table"]/tr[3]/td[2]/text()').extract())
+        
         try:
-            #print(response.xpath('//table[@class="adjust_table"]/tr[3]/td[2]/text()').extract())
+            #招生科目
             subjects = ''
             for s in response.xpath('//table[@class="adjust_table"]/tr[3]/td[2]/text()').extract():    
                 pattern = re.compile(r'\s*',re.S)
@@ -50,29 +49,28 @@ class KaoyaninfoSpider(scrapy.Spider):
             print('error: 招生科目有错误！')
             item['subjects']='error: 招生科目有错误！'
         try:    
-            #//*[@id="pid1"]/tr[1]/td[2]/div/div[1]/div/table/tbody/tr[4]/td[2]
+            #招生年份
             print(response.xpath('//table[@class="adjust_table"]/tr[4]/td[2]/text()').extract()[0])
             item['years'] = response.xpath('//table[@class="adjust_table"]/tr[4]/td[2]/text()').extract()[0]
         except IndexError:
             print('error: 年份有错误！')
             item['years'] = 'error: 年份有错误！'
-        #//*[@id="pid1"]/tr[1]/td[2]/div/div[1]/div/table/tbody/tr[5]/td[2]
-        #//*[@id="pid1"]/tr[1]/td[2]/div/div[2]/div/table/tbody/tr[4]/td[2]
         try:
+            #招生人数
             print(response.xpath('//table[@class="adjust_table"]/tr[5]/td[2]/text()').extract()[0])
             item['pnum'] = response.xpath('//table[@class="adjust_table"]/tr[5]/td[2]/text()').extract()[0]
         except IndexError:
             print('error: 无法显示招生人数')
             item['pnum']='error: 无法显示招生人数'
-        print('+++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    
         print(response.url)
+        #帖子网址
         item['url'] = response.url
-        print('+++++++++++++++++++++++++++++++++++++++++++++++++++++')
         #发帖时间
         print(response.xpath('//div[@class="pls_info"]/em/a/text()').extract()[0])
         item['time'] = response.xpath('//div[@class="pls_info"]/em/a/text()').extract()[0]
-        #//*[@id="pid1"]/tr[3]/td[1]/div/em/a
-        #//*[@id="pid1"]/tr[1]/td[2]/div/div[1]/div/table/tbody/tr[2]/td[2]
+        print('+++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    
         yield item
         
     
